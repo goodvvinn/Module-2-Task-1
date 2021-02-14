566287e06f3d995bfa65af642f4a64fad1f0ee65
goodvvinn <goodvvinn@gmail.com>
2021-02-14 23:32:03 +0200

    preparatory stage

--- Logger/Program.cs
+++ Logger/Program.cs
@@ -2,11 +2,11 @@
 
 namespace Logger
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Run();
         }
     }
 }
---
+++ Logger/Starter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+   public class Starter
+    {
+        public static void Run()
+        {
+            int counter = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+
+            }
+        }
+    }
+}
